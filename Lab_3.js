97ea2c96ce64f19a6837ebb1067dacfa50340952
rachel-trainor <rachel_trainor@byu.net>
2016-02-09 22:41:34 +0000

    Lab 3 commit

---
+++ Lab_3.js
@@ -0,0 +1,41 @@
+angular.module('app', [])
+  .controller('mainCtrl', mainCtrl)
+  .directive('avatar', avatarDirective);
+
+function mainCtrl ($scope) {
+
+  $scope.users = [];
+
+  $scope.addNew = function (user) {
+    $scope.users.push({ 
+      name: user.name,
+      avatarUrl: user.url
+    }); /* [1] */
+    
+    user.name = ''; /* [2] */
+    user.url = '';
+  };
+}
+
+function avatarDirective () {
+  return {
+    scope: {
+      user: '=' /* [1] */
+    },
+    restrict: 'E', /* [2] */
+    template: (
+      '<div class="Avatar">' +
+        '<img ng-src="{{user.avatarUrl}}" />' +
+        '<h4>{{user.name}}</h4>' +
+      '</div>'
+    ), /* [3] */
+    link: link
+  };
+  
+  function link (scope) { /* [4] */
+    if (!scope.user.avatarUrl) {
+      scope.user.avatarUrl = 'http://thealmanac.org/assets/img/default_avatar.png';
+    }
+  }
+
+}
---
+++ v0.js
@@ -0,0 +1,56 @@
+function get(url) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+
+    req.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
+
+function getJSON(url) {
+  return get(url).then(JSON.parse);
+}
+
+get('story.json').then(function(response) {
+  return JSON.parse(response);
+}).then(function(response) {
+  console.log("Yey JSON!", response);
+});
+
+//These are the functions that we'll use later on to interact with the HTML page
+//
+function addHtmlToPage(content) {
+  var div = document.createElement('div');
+  div.innerHTML = content;
+  storyDiv.appendChild(div);
+}
+
+function addTextToPage(content) {
+  var p = document.createElement('p');
+  p.textContent = content;
+  storyDiv.appendChild(p);
+}
+
+
